e62452ee9e34e758100d844dbc480693bb03c007
JVGS1111 <joaoviniciusgs@gmail.com>
2021-11-08 19:37:43 -0300

    implementado header e logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import header from "./header.module.scss"
+import common from "../../styles/common.module.scss"
+import Link from "next/link"
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={`${common.container} ${header.header}`}>
+      <Link href="/">
+        <img src="/images/Logo.svg" alt="logo" />
+      </Link>
+    </header>
+  );
 }
